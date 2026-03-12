621e42f52f79754ffbf129a5a56533ba86a13050
macrijuan <amacri162013@yahoo.com>
2026-03-12 14:21:07 -0300

    idk

--- src/routes/service/get_service.js
+++ src/routes/service/get_service.js
@@ -7,7 +7,6 @@ router.get( "/get_services",
   async( req, res, next ) => {
     try{
       const serv = await Service.findAll({
-        attributes: [ "id", "name", "am", "pm" ],
         include:{ model: Sub_service }
       });
       return res.status( 200 ).json( serv );
